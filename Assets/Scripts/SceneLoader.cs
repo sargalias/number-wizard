533efb68f0e011a82f336f63839e620aeb94a836
Spyros Argalias <sargalias@gmail.com>
2020-02-14 01:10:36 +0000

    feat(Scripts/SceneLoader): Create script

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+    public void LoadNextScene() {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        int nextScene = (currentScene + 1) % 3;
+        SceneManager.LoadScene(nextScene);
+    }
+}
