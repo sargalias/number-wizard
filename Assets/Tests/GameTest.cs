b60aa5c480a46efd23ce03e7d4c145eecd69172c
Spyros Argalias <sargalias@gmail.com>
2020-02-15 03:58:10 +0000

    feat(GameHelper): Add GameHelper files

---
+++ Assets/Tests/GameTest.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests {
+    public class GameTest {
+    }
+}
