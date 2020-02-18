4653e98a71578a2d205d2911c2d8e5f41c68bd57
Spyros Argalias <sargalias@gmail.com>
2020-02-18 16:37:16 +0000

    feat(IGuessBinarySearch): Add interface for GuessBinarySearch

--- Assets/Scripts/GuessBinarySearch.cs
+++ Assets/Scripts/GuessBinarySearch.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 
-public class GuessBinarySearch {
+public class GuessBinarySearch : IGuessBinarySearch {
     private int _low;
     private int _high;
 
