505b0d27245ad10d5ad40591582f8a204e6d90b1
Spyros Argalias <sargalias@gmail.com>
2020-02-20 01:58:16 +0000

    feat(Scrips/Game): Add Game script

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Game : MonoBehaviour {
+    [SerializeField]
+    private Text text;
+
+    private IGuessBinarySearch _gbs;
+    void Start() {
+        _gbs = new GuessBinarySearchWithVariance(1, 1000);
+        updateUI();
+    }
+
+    public void onGuessWasLow() {
+        _gbs.feedbackGuessWasLow();
+        updateUI();
+    }
+
+    public void onGuessWasHigh() {
+        _gbs.feedbackGuessWasHigh();
+        updateUI();
+    }
+
+    private void updateUI() {
+        int newGuess = _gbs.guess();
+        text.text = Convert.ToString(newGuess);
+    }
+}
